b870931f47efb6e288ada8993fd13f55dd12f9fb
Yu Tao <36655406+TonyYu02@users.noreply.github.com>
2025-08-30 10:24:50 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+from tabulate import tabulate
+
+timestamp = int(time.time() * 1000)
+
+authen = {
+    'username': ,
+    'password': ,
+}
+
+session = requests.Session()
+login_page = session.get("https://sso.buaa.edu.cn/login?service=https://yjsxk.buaa.edu.cn/yjsxkapp/sys/xsxkappbuaa/*default/index.do")
+soup = BeautifulSoup(login_page.text, 'html.parser')
+execution_input = soup.find('input', {'name': 'execution'})
+execution_value = execution_input.get('value', '')
+
+login_data = {
+    'username': authen['username'],
+    'password': authen['password'],
+    'type': 'username_password',
+    'submit': 'LOGIN',
+    '_eventId': 'submit',
+    'execution':execution_value
+}
+
+response = session.post("https://sso.buaa.edu.cn/login", data=login_data)
+r=session.get("https://yjsxk.buaa.edu.cn/yjsxkapp/sys/xsxkappbuaa/index.html")
+cookies = session.cookies.get_dict()
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36',
+    'origin': 'https://yjsxk.buaa.edu.cn',
+    'referer' : 'https://yjsxk.buaa.edu.cn/yjsxkapp/sys/xsxkappbuaa/course.html'
+}
+
+cookies = {
+'JSESSIONID':cookies['JSESSIONID'],
+'GS_SESSIONID':cookies['GS_SESSIONID']
+}
+
+base_url="https://yjsxk.buaa.edu.cn/yjsxkapp/sys/xsxkappbuaa/xsxkCourse/loadAllCourseInfo.do?"
+new_url = base_url + "_=" + str(timestamp)
+
+response = requests.get(new_url, headers=headers, cookies=cookies)
+data = response.json()
+n_url=new_url+"&pageSize="+str(data["total"])
+
+course=requests.get(n_url, headers=headers, cookies=cookies)
+courses = course.json()
+
+wids = [
+"37EAB9942C577EA3E0630111FE0A1940",
+]
+wid_set = set(wids)
+
+head = ["课程名称", "容量", "已选"]
+cours=[]
+for course in courses["datas"]:
+    if course["WID"] in wid_set:
+        cours.append([course['KCMC'],course['KXRS'],course['YXXKJGRS']])
+
+print(tabulate(cours, head, tablefmt="fancy_grid"))
