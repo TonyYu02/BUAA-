6a6add1ae20a64c5e8fee18d99951d08d4c69c77
Tony Yu <tonynjupt@gmail.com>
2025-09-01 11:47:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -48,11 +48,12 @@ def query():
 
     bjdm = []
     yxk = {}
+    wids = {}
     for nr in yx["xkjgList"]:
         if nr["BJDM"] not in set(bjdm) and "MXMK" not in nr["BJDM"]:
             bjdm.append(nr["BJDM"])
             yxk[nr["BJDM"]] = nr["YYZ"]
-
+            wids[nr["BJDM"]] = nr["WID"]
     base_url = "https://yjsxk.buaa.edu.cn/yjsxkapp/sys/xsxkappbuaa/xsxkCourse/loadAllCourseInfo.do?"
     timestamp = int(time.time() * 1000)
     new_url = base_url + "_=" + str(timestamp) + "&pageSize=3000"
@@ -62,27 +63,52 @@ def query():
 
     bjdm_set = set(bjdm)
 
-    head = ["课程名称", "授课教师", "学分", "线下容量", "线上容量", "已选", "意愿值"]
+    head = ["编号","课程名称", "授课教师", "学分", "线下容量", "线上容量", "已选", "意愿值"]
     cours = []
+    bhdic={}
+    i = 1
     for course in courses["datas"]:
         if course["BJDM"] in bjdm_set:
             cours.append(
-                [course['KCMC'], course['RKJS'], course['KCXF'], course['KXRS'], course['XSRL'], course['YXXKJGRS'],
+                [i, course['KCMC'], course['RKJS'], course['KCXF'], course['KXRS'], course['XSRL'], course['YXXKJGRS'],
                  yxk[course["BJDM"]]])
-
+            bhdic[i]= wids[course["BJDM"]]
+            i=i+1
     print(tabulate(cours, head, tablefmt="fancy_grid"))
+    return bhdic
 
 
-def loop():
-	a = int(input("按1刷新，其余键退出："))
-	if a == 1:
-		os.system('cls')
-		query()
-		loop()
-	else:
-		session.close()
-		exit(0)
+def loop(bhdic):
+    a = int(input("按1刷新，按2修改对应意愿值，其余键退出："))
+    if a == 1:
+        os.system('cls')
+        k = query()
+        loop(k)
+    elif a == 2:
+        i = int(input("修改意愿值，请输入已选课程编号："))
+        j= int(input("修改后的值为："))
+        xg="https://yjsxk.buaa.edu.cn/yjsxkapp/sys/xsxkappbuaa/xsxkYxxk/changeYyz.do?_="
+        timestamp = int(time.time() * 1000)
+        xg=xg+str(timestamp)
+        xg_data={
+            "wid" : bhdic[i],
+            "yyz" : j
+        }
+        r= session.post(xg, data=xg_data)
+        rj=r.json()
+        print(rj["code"])
+        os.system('cls')
+        if rj["code"] == 1:
+            print("修改成功。")
+        else:
+            print(rj["msg"])
+        k = query()
+        loop(k)
+
+    else:
+        session.close()
+        exit(0)
 
 if __name__ == "__main__":
-	query()
-	loop()
+	k = query()
+	loop(k)
