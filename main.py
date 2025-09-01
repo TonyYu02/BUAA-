6ef80a48b0d0f61090c3f2b0f0ee0465326fd0b5
Tony Yu <tonynjupt@gmail.com>
2025-09-01 10:17:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import time
 import requests
 from bs4 import BeautifulSoup
@@ -38,33 +39,50 @@ login_data = {
 
 response = session.post("https://sso.buaa.edu.cn/login", data=login_data)
 
-yx_url="https://yjsxk.buaa.edu.cn/yjsxkapp/sys/xsxkappbuaa/xsxkCourse/loadKbxx.do?sfyx=1&sfjzsyzz=1&_="
-timestamp = int(time.time() * 1000)
-yx_url=yx_url+str(timestamp)
-response = get_web(yx_url)
-yx=response.json()
+def query():
+    yx_url = "https://yjsxk.buaa.edu.cn/yjsxkapp/sys/xsxkappbuaa/xsxkCourse/loadKbxx.do?sfyx=1&sfjzsyzz=1&_="
+    timestamp = int(time.time() * 1000)
+    yx_url = yx_url + str(timestamp)
+    response = get_web(yx_url)
+    yx = response.json()
 
-bjdm=[]
-yxk = {}
-for nr in yx["xkjgList"]:
-	if nr["BJDM"] not in set(bjdm) and "MXMK" not in nr["BJDM"]:
-		bjdm.append(nr["BJDM"])
-		yxk[nr["BJDM"]] = nr["YYZ"]
+    bjdm = []
+    yxk = {}
+    for nr in yx["xkjgList"]:
+        if nr["BJDM"] not in set(bjdm) and "MXMK" not in nr["BJDM"]:
+            bjdm.append(nr["BJDM"])
+            yxk[nr["BJDM"]] = nr["YYZ"]
 
-base_url="https://yjsxk.buaa.edu.cn/yjsxkapp/sys/xsxkappbuaa/xsxkCourse/loadAllCourseInfo.do?"
-timestamp = int(time.time() * 1000)
-new_url = base_url + "_=" + str(timestamp)+"&pageSize=3000"
+    base_url = "https://yjsxk.buaa.edu.cn/yjsxkapp/sys/xsxkappbuaa/xsxkCourse/loadAllCourseInfo.do?"
+    timestamp = int(time.time() * 1000)
+    new_url = base_url + "_=" + str(timestamp) + "&pageSize=3000"
 
-course=get_web(new_url)
-courses = course.json()
+    course = get_web(new_url)
+    courses = course.json()
 
-bjdm_set = set(bjdm)
+    bjdm_set = set(bjdm)
 
-head = ["课程名称", "授课教师", "学分", "线下容量", "线上容量", "已选", "意愿值"]
-cours=[]
-for course in courses["datas"]:
-    if course["BJDM"] in bjdm_set:
-        cours.append([course['KCMC'], course['RKJS'], course['KCXF'], course['KXRS'], course['XSRL'], course['YXXKJGRS'],yxk[course["BJDM"]]])
+    head = ["课程名称", "授课教师", "学分", "线下容量", "线上容量", "已选", "意愿值"]
+    cours = []
+    for course in courses["datas"]:
+        if course["BJDM"] in bjdm_set:
+            cours.append(
+                [course['KCMC'], course['RKJS'], course['KCXF'], course['KXRS'], course['XSRL'], course['YXXKJGRS'],
+                 yxk[course["BJDM"]]])
 
-print(tabulate(cours, head, tablefmt="fancy_grid"))
-session.close()
+    print(tabulate(cours, head, tablefmt="fancy_grid"))
+
+
+def loop():
+	a = int(input("按1刷新，其余键退出："))
+	if a == 1:
+		os.system('cls')
+		query()
+		loop()
+	else:
+		session.close()
+		exit(0)
+
+if __name__ == "__main__":
+	query()
+	loop()
